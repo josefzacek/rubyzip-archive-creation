7d25db58b85900c9bde2d60d6c7309df1fd1791e
Josef Zacek <info@josefzacek.com>
2016-05-14 00:41:32 +0100

    download_zip method in person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ActiveRecord::Base
+  def self.download_zip(people)
+    compressed_file = Zip::OutputStream.write_buffer(::StringIO.new('')) do |zos|
+      people.each do |person|
+        zos.put_next_entry "#{person.name}-#{person.id}.csv"
+        zos.print "#{person.name}, #{person.age}, #{person.occupation}"
+      end
+    end
+    compressed_file.rewind
+    compressed_file
+  end
 end
