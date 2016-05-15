128af86f4152fef94316ad1ae64bb469e3545093
Josef Zacek <info@josefzacek.com>
2016-05-15 21:49:08 +0100

    fix compatibility for rubyzip gem

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ require 'zip'
 
 class Person < ActiveRecord::Base
   def self.download_zip(people)
-    compressed_file = Zip::OutputStream.write_buffer(::StringIO.new('')) do |zos|
+    compressed_file = Zip::OutputStream.write_buffer do |zos|
       people.each do |person|
         zos.put_next_entry "#{person.name}-#{person.id}.csv"
         zos.print "#{person.name}, #{person.age}, #{person.occupation}"
